590bbbb43ba0b54e8c799b9b266ac9251d65f1b9
govindansriram <sriramgovindanwork@gmail.com>
2024-07-21 16:47:54 -0700

    updated queue to be more eficent

--- queue/queue.go
+++ queue/queue.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 type Queue struct {
@@ -10,16 +11,24 @@ type Queue struct {
 	lock           sync.Mutex
 	maxMessages    uint32
 	maxMessageSize *uint32
+	readPos        atomic.Int32
+	writePos       atomic.Int32
 }
 
-func Init(maxMessCount uint32, maxMessSize *uint32) Queue {
+func isNil(slice [][]byte) {
+	if slice == nil {
+		panic("backingSlice cannot be nil, please use NewQueue method to create a queue")
+	}
+}
+
+func NewQueue(maxMessCount uint32, maxMessSize *uint32) Queue {
 	var split = maxMessCount
 
-	if maxMessCount > 3 {
-		split = maxMessCount / 3
+	if maxMessCount > 10 {
+		split = maxMessCount / 10
 	}
 
-	bs := make([][]byte, 0, split)
+	bs := make([][]byte, split)
 
 	return Queue{
 		backingSlice:   bs,
@@ -28,36 +37,39 @@ func Init(maxMessCount uint32, maxMessSize *uint32) Queue {
 	}
 }
 
-func (q *Queue) Len() int {
-
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
-	return len(q.backingSlice)
+func (q *Queue) Len() int32 {
+	return q.writePos.Load() - q.readPos.Load()
 }
 
 func (q *Queue) Pop() ([]byte, error) {
+	isNil(q.backingSlice)
 	q.lock.Lock()
 	defer q.lock.Unlock()
 
-	if len(q.backingSlice) == 0 {
+	if q.Len() == 0 {
 		return nil, errors.New("cannot pop from empty slice")
 	}
 
-	response := q.backingSlice[0]
+	readPos := q.readPos.Load()
 
-	offByOne := make([][]byte, len(q.backingSlice)-1, cap(q.backingSlice))
-	copy(offByOne, q.backingSlice[1:])
-	q.backingSlice = offByOne
+	response := q.backingSlice[readPos]
+	q.backingSlice[readPos] = []byte{}
+	q.readPos.Add(1)
 
 	return response, nil
 }
 
 func (q *Queue) Push(message []byte) (uint32, error) {
+	isNil(q.backingSlice)
+
+	if len(message) == 0 {
+		panic("message is empty")
+	}
+
 	q.lock.Lock()
 	defer q.lock.Unlock()
 
-	if len(q.backingSlice) >= int(q.maxMessages) {
+	if q.Len() >= int32(q.maxMessages) {
 		return 0, errors.New("queue is full")
 	}
 
@@ -65,7 +77,59 @@ func (q *Queue) Push(message []byte) (uint32, error) {
 		return 0, errors.New("message is too large")
 	}
 
-	q.backingSlice = append(q.backingSlice, message)
+	if q.writePos.Load() == int32(cap(q.backingSlice)) {
+		q.adjust()
+	}
 
-	return uint32(len(q.backingSlice)), nil
+	writePos := q.writePos.Load()
+
+	if writePos == int32(cap(q.backingSlice)) {
+		var dataSlice [][]byte
+
+		if int(q.maxMessages/2) < cap(q.backingSlice) {
+			dataSlice = make([][]byte, cap(q.backingSlice)*2)
+		} else {
+			dataSlice = make([][]byte, q.maxMessages)
+		}
+
+		copy(dataSlice, q.backingSlice)
+		dataSlice[writePos] = message
+		q.backingSlice = dataSlice
+	} else {
+		q.backingSlice[writePos] = message
+	}
+
+	q.writePos.Add(1)
+	return uint32(q.Len()), nil
+}
+
+func (q *Queue) adjust() {
+	isNil(q.backingSlice)
+
+	var startingPos = -1
+	index := 0
+
+	for startingPos < 0 || index < len(q.backingSlice) {
+		messLen := len(q.backingSlice[index])
+
+		if index == 0 && messLen >= 0 {
+			return
+		}
+
+		if messLen >= 0 {
+			startingPos = index
+		}
+
+		index++
+	}
+
+	index = 0
+	for i := startingPos; i < len(q.backingSlice); i++ {
+		q.backingSlice[index] = q.backingSlice[i]
+		q.backingSlice[i] = []byte{}
+		index++
+	}
+
+	q.writePos.Store(int32(index))
+	q.readPos.Store(0)
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,23 +1,204 @@
 package queue
 
 import (
-	"fmt"
-	"sync/atomic"
+	"bytes"
+	"math"
+	"math/rand/v2"
+	"sync"
 	"testing"
 )
 
-func updateVal(v *atomic.Value) {
+func generateBytes() []byte {
+	length := rand.IntN(8) + 1
+	buffer := make([]byte, length)
 
-	val := v.Load().(int)
-	v.Store(val + 1)
+	for idx := range buffer {
+		buffer[idx] = uint8(rand.IntN(math.MaxUint8))
+	}
+
+	return buffer
 }
 
-func Test_q(t *testing.T) {
+func TestQueue_Pop(t *testing.T) {
+	const routines = 100
 
-	val := atomic.Value{}
-	val.Store(1)
+	fullBuffer := make([][]byte, routines)
 
-	updateVal(&val)
+	for idx := range routines {
+		fullBuffer[idx] = generateBytes()
+	}
 
-	fmt.Println(val.Load())
+	q := Queue{
+		backingSlice:   fullBuffer,
+		maxMessageSize: nil,
+		maxMessages:    routines,
+	}
+
+	q.readPos.Store(0)
+	q.writePos.Store(routines)
+
+	wg := sync.WaitGroup{}
+
+	for range routines {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := q.Pop()
+
+			if err != nil {
+				t.Error(err)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if q.Len() != 0 {
+		t.Error("queue is not empty")
+	}
+}
+
+func TestQueue_Push(t *testing.T) {
+	const routines = 50
+
+	fullBuffer := make([][]byte, routines/10)
+
+	q := Queue{
+		backingSlice:   fullBuffer,
+		maxMessageSize: nil,
+		maxMessages:    routines,
+	}
+
+	q.readPos.Store(0)
+	q.writePos.Store(0)
+
+	wg := sync.WaitGroup{}
+
+	byteSlice := make([][]byte, routines)
+
+	for idx := range routines {
+		byteData := generateBytes()
+		byteSlice[idx] = byteData
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			_, err := q.Push(byteData)
+			if err != nil {
+				t.Error(err)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if q.Len() != routines {
+		t.Error("queue is not full")
+	}
+
+	if len(q.backingSlice) != routines {
+		t.Error("queue is not full")
+	}
+
+	compareBytes(t, byteSlice, q.backingSlice)
+
+}
+
+func compareBytes(t *testing.T, control, test [][]byte) {
+	for _, i := range control {
+		found := false
+		for _, j := range test {
+			if bytes.Equal(i, j) {
+				found = true
+			}
+		}
+
+		if !found {
+			t.Error("byte was not added")
+		}
+	}
+}
+
+/*
+TestQueue_All
+
+simulate 100 concurrent reads and 100 concurrent writes in random order
+*/
+func TestQueue_All(t *testing.T) {
+	const mx = uint32(100)
+	queue := NewQueue(mx, nil)
+
+	wgPop := sync.WaitGroup{}
+	dataChan := make(chan []byte)
+	failedChan := make(chan struct{})
+
+	popper := func() {
+		data, err := queue.Pop()
+		if err == nil {
+			dataChan <- data
+		} else {
+			failedChan <- struct{}{}
+		}
+	}
+
+	pusher := func(data []byte) {
+		_, err := queue.Push(data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	var scheduledPop int
+	var scheduledPush int
+
+	opts := []int{-1, 1}
+
+	control := make([][]byte, 0, mx)
+	test := make([][]byte, 0, mx)
+
+	for range mx * 2 {
+		num := opts[rand.IntN(2)]
+
+		if (num == -1 && scheduledPop == int(mx)) || (num == 1 && scheduledPush == int(mx)) {
+			num *= -1
+		}
+
+		if num == -1 {
+			wgPop.Add(1)
+			go popper()
+			scheduledPop++
+		} else {
+			byt := generateBytes()
+			control = append(control, byt)
+			go pusher(byt)
+			scheduledPush++
+		}
+	}
+
+	go func() {
+		for {
+			data := <-dataChan
+			test = append(test, data)
+			wgPop.Done()
+		}
+	}()
+
+	go func() {
+		wgPop.Wait()
+		close(failedChan)
+	}()
+
+	for range failedChan {
+		go popper()
+	}
+
+	if len(control) != len(test) {
+		t.Error("collected arrays are not the same")
+	}
+
+	compareBytes(t, control, test)
+
+	if queue.Len() != 0 {
+		t.Error("queue is not empty")
+	}
 }
