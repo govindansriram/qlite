cba7b9ca3c8cb997719ef30dd048d7cca5078f49
govindansriram <sriramgovindanwork@gmail.com>
2024-07-21 18:25:20 -0700

    fixed queue function adjust, and finsihed queue tests

--- queue/queue.go
+++ queue/queue.go
@@ -1,3 +1,15 @@
+/*
+Author: Sriram Govindan
+Date: 2024-07-21
+Description: a fast in memory queue that is thread safe.
+
+How does it work?
+
+a queue is backed by a slice, to increase speed we never append to a slice we only assign (s[1] = 1). Because you can
+both push and pop from a slice we need to keep track of the current write and read position. If we require more space,
+we create a new slice with twice the capacity and copy over the data.
+*/
+
 package queue
 
 import (
@@ -15,12 +27,22 @@ type Queue struct {
 	writePos       atomic.Int32
 }
 
+/*
+isNil
+
+checks if the backing slice of a queue is nil (forces users to use the NewQueue method)
+*/
 func isNil(slice [][]byte) {
 	if slice == nil {
 		panic("backingSlice cannot be nil, please use NewQueue method to create a queue")
 	}
 }
 
+/*
+NewQueue
+
+creates a new Queue
+*/
 func NewQueue(maxMessCount uint32, maxMessSize *uint32) Queue {
 	var split = maxMessCount
 
@@ -37,10 +59,20 @@ func NewQueue(maxMessCount uint32, maxMessSize *uint32) Queue {
 	}
 }
 
+/*
+Len
+
+gets the length of the queue
+*/
 func (q *Queue) Len() int32 {
 	return q.writePos.Load() - q.readPos.Load()
 }
 
+/*
+Pop
+
+removes the first element from the Queue
+*/
 func (q *Queue) Pop() ([]byte, error) {
 	isNil(q.backingSlice)
 	q.lock.Lock()
@@ -53,12 +85,17 @@ func (q *Queue) Pop() ([]byte, error) {
 	readPos := q.readPos.Load()
 
 	response := q.backingSlice[readPos]
-	q.backingSlice[readPos] = []byte{}
+	q.backingSlice[readPos] = []byte{} // mark the popped position as empty
 	q.readPos.Add(1)
 
 	return response, nil
 }
 
+/*
+Push
+
+appends an element to the end of the Queue, returns the position of the element in the queue
+*/
 func (q *Queue) Push(message []byte) (uint32, error) {
 	isNil(q.backingSlice)
 
@@ -87,8 +124,10 @@ func (q *Queue) Push(message []byte) (uint32, error) {
 		var dataSlice [][]byte
 
 		if int(q.maxMessages/2) < cap(q.backingSlice) {
+			// double the capacity if  double the current capacity is under the max message limit
 			dataSlice = make([][]byte, cap(q.backingSlice)*2)
 		} else {
+			// set the capacity to the max
 			dataSlice = make([][]byte, q.maxMessages)
 		}
 
@@ -103,27 +142,43 @@ func (q *Queue) Push(message []byte) (uint32, error) {
 	return uint32(q.Len()), nil
 }
 
+/*
+adjust
+
+as Pop's occur the indexes that have been popped will be marked as empty. Since pops are sequential we can assume
+that after some time there will be a continuous sequence of empty indexes starting from the beginning of the slice.
+
+adjust ensures that values that have not been popped are shifted back to the front taking up those empty spaces.
+*/
 func (q *Queue) adjust() {
 	isNil(q.backingSlice)
 
 	var startingPos = -1
 	index := 0
 
-	for startingPos < 0 || index < len(q.backingSlice) {
+	for index < len(q.backingSlice) {
 		messLen := len(q.backingSlice[index])
 
-		if index == 0 && messLen >= 0 {
+		if index == 0 && messLen > 0 {
+			// there are no empty spaces in the slice
 			return
 		}
 
-		if messLen >= 0 {
+		if messLen > 0 {
 			startingPos = index
+			break
 		}
 
 		index++
 	}
 
+	if startingPos == -1 {
+		// entire slice is empty
+		startingPos = index
+	}
+
 	index = 0
+	// shift non popped data back to the front
 	for i := startingPos; i < len(q.backingSlice); i++ {
 		q.backingSlice[index] = q.backingSlice[i]
 		q.backingSlice[i] = []byte{}
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"bytes"
+	"fmt"
 	"math"
 	"math/rand/v2"
 	"sync"
@@ -22,86 +23,158 @@ func generateBytes() []byte {
 func TestQueue_Pop(t *testing.T) {
 	const routines = 100
 
-	fullBuffer := make([][]byte, routines)
+	t.Run("concurrent pops", func(t *testing.T) {
+		fullBuffer := make([][]byte, routines)
 
-	for idx := range routines {
-		fullBuffer[idx] = generateBytes()
-	}
+		for idx := range routines {
+			fullBuffer[idx] = generateBytes()
+		}
 
-	q := Queue{
-		backingSlice:   fullBuffer,
-		maxMessageSize: nil,
-		maxMessages:    routines,
-	}
+		q := Queue{
+			backingSlice:   fullBuffer,
+			maxMessageSize: nil,
+			maxMessages:    routines,
+		}
 
-	q.readPos.Store(0)
-	q.writePos.Store(routines)
+		q.readPos.Store(0)
+		q.writePos.Store(routines)
 
-	wg := sync.WaitGroup{}
+		wg := sync.WaitGroup{}
 
-	for range routines {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			_, err := q.Pop()
+		for range routines {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err := q.Pop()
 
-			if err != nil {
-				t.Error(err)
-			}
-		}()
-	}
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+		}
 
-	wg.Wait()
+		wg.Wait()
 
-	if q.Len() != 0 {
-		t.Error("queue is not empty")
-	}
+		if q.Len() != 0 {
+			t.Error("queue is not empty")
+		}
+	})
+
+	t.Run("pop from empty queue", func(t *testing.T) {
+		fullBuffer := make([][]byte, routines)
+
+		q := Queue{
+			backingSlice:   fullBuffer,
+			maxMessageSize: nil,
+			maxMessages:    routines,
+		}
+
+		q.readPos.Store(0)
+		q.writePos.Store(0)
+
+		if _, err := q.Pop(); err == nil {
+			t.Error("popped from empty queue")
+		}
+	})
 }
 
 func TestQueue_Push(t *testing.T) {
 	const routines = 50
 
-	fullBuffer := make([][]byte, routines/10)
+	t.Run("concurrent push", func(t *testing.T) {
+		fullBuffer := make([][]byte, routines/10)
 
-	q := Queue{
-		backingSlice:   fullBuffer,
-		maxMessageSize: nil,
-		maxMessages:    routines,
-	}
+		q := Queue{
+			backingSlice:   fullBuffer,
+			maxMessageSize: nil,
+			maxMessages:    routines,
+		}
 
-	q.readPos.Store(0)
-	q.writePos.Store(0)
+		q.readPos.Store(0)
+		q.writePos.Store(0)
 
-	wg := sync.WaitGroup{}
+		wg := sync.WaitGroup{}
 
-	byteSlice := make([][]byte, routines)
+		byteSlice := make([][]byte, routines)
 
-	for idx := range routines {
-		byteData := generateBytes()
-		byteSlice[idx] = byteData
-		wg.Add(1)
+		for idx := range routines {
+			byteData := generateBytes()
+			byteSlice[idx] = byteData
+			wg.Add(1)
 
-		go func() {
-			defer wg.Done()
-			_, err := q.Push(byteData)
-			if err != nil {
-				t.Error(err)
-			}
-		}()
-	}
+			go func() {
+				defer wg.Done()
+				_, err := q.Push(byteData)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+		}
 
-	wg.Wait()
+		wg.Wait()
 
-	if q.Len() != routines {
-		t.Error("queue is not full")
-	}
+		if q.Len() != routines {
+			t.Error("queue is not full")
+		}
 
-	if len(q.backingSlice) != routines {
-		t.Error("queue is not full")
-	}
+		if len(q.backingSlice) != routines {
+			t.Error("queue is not full")
+		}
 
-	compareBytes(t, byteSlice, q.backingSlice)
+		compareBytes(t, byteSlice, q.backingSlice)
 
+		if cap(q.backingSlice) != routines {
+			t.Error("capacity does not match")
+		}
+	})
+
+	t.Run("push to full queue", func(t *testing.T) {
+
+		fullBuffer := make([][]byte, routines)
+
+		q := Queue{
+			backingSlice:   fullBuffer,
+			maxMessageSize: nil,
+			maxMessages:    routines,
+		}
+
+		q.readPos.Store(0)
+		q.writePos.Store(50)
+
+		_, err := q.Push(generateBytes())
+
+		if err == nil {
+			t.Error("queue should not have accepted the request")
+		}
+	})
+
+	t.Run("push very large message", func(t *testing.T) {
+
+		fullBuffer := make([][]byte, routines)
+
+		maxSize := uint32(3)
+
+		q := Queue{
+			backingSlice:   fullBuffer,
+			maxMessageSize: &maxSize,
+			maxMessages:    routines,
+		}
+
+		q.readPos.Store(0)
+		q.writePos.Store(0)
+
+		_, err := q.Push([]byte{1, 2})
+
+		if err != nil {
+			t.Error("queue should have accepted the request")
+		}
+
+		_, err = q.Push([]byte{1, 2, 3, 4})
+
+		if err == nil {
+			t.Error("queue should not have accepted the request")
+		}
+	})
 }
 
 func compareBytes(t *testing.T, control, test [][]byte) {
@@ -201,4 +274,99 @@ func TestQueue_All(t *testing.T) {
 	if queue.Len() != 0 {
 		t.Error("queue is not empty")
 	}
+}
+
+func Test_adjust(t *testing.T) {
+
+	t.Run("test no shifting", func(t *testing.T) {
+		backing := [][]byte{
+			generateBytes(),
+			generateBytes(),
+			generateBytes(),
+			generateBytes(),
+			generateBytes(),
+		}
+
+		q := Queue{
+			backingSlice: backing,
+			maxMessages:  5,
+		}
+
+		q.readPos.Store(5)
+		q.writePos.Store(5)
+
+		q.adjust()
+
+		compareBytes(t, backing, q.backingSlice)
+	})
+
+	t.Run("test shift", func(t *testing.T) {
+		backing := [][]byte{
+			{},
+			{},
+			generateBytes(),
+			generateBytes(),
+			generateBytes(),
+		}
+
+		dupe := make([][]byte, cap(backing))
+		copy(dupe, backing)
+
+		q := Queue{
+			backingSlice: dupe,
+			maxMessages:  5,
+		}
+
+		q.readPos.Store(5)
+		q.writePos.Store(5)
+
+		q.adjust()
+
+		if q.readPos.Load() != 0 {
+			fmt.Println(q.readPos.Load())
+			t.Error("read position was not reset")
+		}
+
+		if q.writePos.Load() != 3 {
+			fmt.Println(q.writePos.Load(), q.backingSlice)
+			t.Error("write position was not reset")
+		}
+
+		eq1 := bytes.Equal(backing[2], q.backingSlice[0])
+		eq2 := bytes.Equal(backing[3], q.backingSlice[1])
+		eq3 := bytes.Equal(backing[4], q.backingSlice[2])
+
+		if !eq1 || !eq2 || !eq3 {
+			t.Error("incorrect shift")
+		}
+	})
+
+	t.Run("shift empty slice", func(t *testing.T) {
+		backing := [][]byte{
+			{},
+			{},
+			{},
+			{},
+			{},
+		}
+
+		q := Queue{
+			backingSlice: backing,
+			maxMessages:  5,
+		}
+
+		q.readPos.Store(5)
+		q.writePos.Store(5)
+
+		q.adjust()
+
+		if q.writePos.Load() != 0 {
+			t.Error("incorrect shift")
+		}
+
+		if q.readPos.Load() != 0 {
+			t.Error("incorrect shift")
+		}
+
+	})
 }
